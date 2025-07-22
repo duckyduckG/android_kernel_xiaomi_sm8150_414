993491aed70a94e5464949a2f4efd33385e24daa
duckyduckG <duckyduckg65@gmail.com>
2025-07-22 07:01:26 +0500

    Revert "drivers: gpu: drm: msm: Add R_EARLY_EVENT_BLANK notification"
    This reverts commit 7746b1d3b4b6b744644c72ee92b6f84bdf959fba.

--- include/linux/msm_drm_notify.h
+++ include/linux/msm_drm_notify.h
@@ -19,8 +19,6 @@
 #define MSM_DRM_EVENT_BLANK			0x01
 /* A hardware display blank early change occurred */
 #define MSM_DRM_EARLY_EVENT_BLANK		0x02
-/* A hardware display blank early change occurred */
-#define MSM_DRM_R_EARLY_EVENT_BLANK		0x03
 
 enum {
 	/* panel: power on */
